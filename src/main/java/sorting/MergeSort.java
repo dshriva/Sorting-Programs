aa363e6c973b49941b62892ef515ee7f9bef84a8
Piyush <me.piyush89@gmail.com>
2018-01-17 23:46:31 -0500

    Minor change in merge sort

--- src/main/java/sorting/MergeSort.java
+++ src/main/java/sorting/MergeSort.java
@@ -3,6 +3,7 @@ package sorting;
 public class MergeSort {
     public static void main(String[] args){
         System.out.println("Hello World!");
+		System.out.println("This is merge sort program!");
     }
 
 }
