a1e671f3c8ca47bfa47a838ed94524110e8b0299
dshriva <divya.shrivastava2014@gmail.com>
2018-01-17 22:12:46 -0600

    Added main function

--- src/main/java/sorting/InsertionSort.java
+++ src/main/java/sorting/InsertionSort.java
@@ -4,5 +4,8 @@ package sorting;
  * created by divya at 1/17/2018
  */
 public class InsertionSort {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
 
 }
